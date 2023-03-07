9f082d8afdbcd94db252d0a40b785e7a1f81c91f
denys-natanael-clear <denys.natanael.clear@gmail.com>
2023-03-07 12:57:03 -0300

    feat: lessons 134 and 135: recording audio captured by the microphone and creating an event to view the recording time

---
+++ src/util/Firebase.js
@@ -0,0 +1,22 @@
+export class Firebase {
+
+    constructor () {
+
+        this.init()
+
+    }
+
+    init () {
+
+        const firebaseConfig = {
+            apiKey: "AIzaSyD4wDeqlpanqLFwQ-J-y8s7VyoFfnPTftE",
+            authDomain: "whatsapp-clone-bcd3f.firebaseapp.com",
+            projectId: "whatsapp-clone-bcd3f",
+            storageBucket: "whatsapp-clone-bcd3f.appspot.com",
+            messagingSenderId: "895279050794",
+            appId: "1:895279050794:web:0e6b4d799e847304f9d353"
+          };
+
+    }
+
+}
