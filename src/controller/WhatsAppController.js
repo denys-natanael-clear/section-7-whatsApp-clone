52d90e3d7bafc8fdd4a617844a4f71c1ccd750b3
denys-natanael-clear <denys.natanael.clear@gmail.com>
2023-03-10 10:41:43 -0300

    feat: lessons 157 and 158: updating profile data and updating the last manually sent message

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -8,6 +8,7 @@ import { Chat } from '../model/Chat.js'
 import { Message } from '../model/Message.js'
 import { Base64 } from '../util/Base64.js'
 import { ContactsController } from './ContactsController.js'
+import { Upload } from '../util/Upload.js'
 
 
 export class WhatsAppController {
@@ -108,7 +109,7 @@ export class WhatsAppController {
                         <span dir="auto" title="${contact.name}" class="_1wjpf">${contact.name}</span>
                     </div>
                     <div class="_3Bxar">
-                        <span class="_3T2VG">${contact.lastMessageTime}</span>
+                        <span class="_3T2VG">${Format.timeStampToTime(contact.lastMessageTime)}</span>
                     </div>
                 </div>
                 <div class="_1AwDx">
@@ -421,6 +422,27 @@ export class WhatsAppController {
         this.el.photoContainerEditProfile.on('click', e => {
 
             this.el.inputProfilePhoto.click('')
+
+        })
+
+        this.el.inputProfilePhoto.on('change', e => {
+
+            if (this.el.inputProfilePhoto.files.length > 0) {
+
+                let file = this.el.inputProfilePhoto.files[0]
+
+                 Upload.send(file, this._user.email).then(downloadURL => {
+
+                    this._user.photo = downloadURL
+                    this._user.save().then(() => {
+
+                        this.el.btnClosePanelEditProfile.click()
+
+                    })
+
+                 })
+
+            }
 
         })
 
