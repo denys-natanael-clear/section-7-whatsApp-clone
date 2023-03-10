0c8b4749f0f5565fb530417dc80d68dbc10c530e
denys-natanael-clear <denys.natanael.clear@gmail.com>
2023-03-10 10:19:35 -0300

    feat: lessons 155 and 156: sending message to attached contact and recording and sending audio as attachment

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -1,8 +1,8 @@
 import { ClassEvent } from "../util/ClassEvent"
 
-export class MicrophoneController extends ClassEvent{
+export class MicrophoneController extends ClassEvent {
 
-    constructor () {
+    constructor() {
 
         super()
 
@@ -25,7 +25,7 @@ export class MicrophoneController extends ClassEvent{
 
     }
 
-    stop () {
+    stop() {
 
         this._stream.getTracks().forEach(track => {
             track.stop()
@@ -33,53 +33,53 @@ export class MicrophoneController extends ClassEvent{
 
     }
 
-    isAvailable () {
+    isAvailable() {
 
         return this._available
 
     }
 
-    startRecorder () {
+    startRecorder(){
 
-        if (this.isAvailable()) {
-
+        if(this.isAvailable){
             this._mediaRecorder = new MediaRecorder(this._stream, {
                 mimeType: this._mimeType
-            })
-
-            this._recorderChunks = []
-
+            });
+            this._recordedChunks = [];
             this._mediaRecorder.addEventListener('dataavailable', e => {
-
-                if(e.data.size > 0) this._recorderChunks.push(e.data)
-
-            })
+                if(e.data.size> 0){
+                    this._recordedChunks.push(e.data)
+                }
+            });
 
             this._mediaRecorder.addEventListener('stop', e => {
-
-                let blob = new Blob(this._recorderChunks, {
-                    type: this._mimeType
+                let blob = new Blob(this._recordedChunks, {
+                    type: this.mimeType
                 })
-
+                
                 let filename = `rec${Date.now()}.webm`
 
                 let file = new File([blob], filename, {
                     type: this._mimeType,
                     lastModified: Date.now()
-                })
-
+                });
                 console.log('file', file)
 
-            })
+                let reader = new FileReader()
+                reader.onload = e =>{
+                    let audio = new Audio(reader.result)
+                    audio.play()
+                }
+                reader.readAsDataURL(file)
+            });
 
             this._mediaRecorder.start()
             this.startTimer()
-
-        }
+        }       
 
     }
 
-    stopRecorder () {
+    stopRecorder() {
 
         if (this.isAvailable()) {
 
@@ -91,19 +91,19 @@ export class MicrophoneController extends ClassEvent{
 
     }
 
-    startTimer () {
+    startTimer() {
 
         let start = Date.now()
 
         this._recordMicrophoneInterval = setInterval(() => {
 
             this.trigger('recordtimer', (Date.now() - start))
- 
+
         }, 100)
 
     }
 
-    stopTimer () {
+    stopTimer() {
 
         clearInterval(this._recordMicrophoneInterval)
 
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -207,6 +207,8 @@ export class WhatsAppController {
 
                     let me = (data.from === this._user.email)
 
+                    let view = message.getViewElement(me)
+
                     if (!this.el.panelMessagesContainer.querySelector('#_' + data.id)) {
 
                         if (!me) {
@@ -219,17 +221,15 @@ export class WhatsAppController {
 
                         }
 
-                        let view = message.getViewElement(me)
-
                         this.el.panelMessagesContainer.appendChild(view)
 
                     } else {
 
-                        let view = message.getViewElement(me)
+                        let parent = this.el.panelMessagesContainer.querySelector('#_' + data.id).parentNode
 
-                        this.el.panelMessagesContainer.querySelector('#_' + data.id).innerHTML = view.innerHTML
+                        parent.replaceChild(view, this.el.panelMessagesContainer.querySelector('#_' + data.id))
 
-                    } 
+                    }
 
 
                     if (this.el.panelMessagesContainer.querySelector('#_' + data.id) && me) {
@@ -237,7 +237,35 @@ export class WhatsAppController {
                         let msgEl = this.el.panelMessagesContainer.querySelector('#_' + data.id)
 
                         msgEl.querySelector('.message-status').innerHTML = message.getStatusViewElement().outerHTML
-                    
+
+                    }
+
+                    if (message.type === 'contact') {
+
+                        view.querySelector('.btn-message-send').on('click', e => {
+
+                            Chat.createIfNoteExists(this._user.email, message.content.email).then(chat => {
+
+                                let contact = new User(message.content.email)
+
+                                contact.on('datachange', data => {
+
+                                    contact.chatId = chat.id
+
+                                    this._user.addContact(contact)
+
+                                    this._user.chatId = chat.id
+
+                                    contact.addContact(this._user)
+
+                                    this.setActiveChat(contact)
+
+                                })
+
+                            })
+
+                        })
+
                     }
 
                 })
@@ -689,13 +717,13 @@ export class WhatsAppController {
             } else {
 
                 Message.sendDocument(
-                    this._contactActive.chatId, 
+                    this._contactActive.chatId,
                     this._user.email, file)
 
-                    
-                }
-                
-                this.el.btnClosePanelDocumentPreview.click()
+
+            }
+
+            this.el.btnClosePanelDocumentPreview.click()
 
 
         })
@@ -704,12 +732,12 @@ export class WhatsAppController {
 
             this._contactsController = new ContactsController(this.el.modalContacts, this._user);
 
-            this._contactsController.on('select', contact =>{
+            this._contactsController.on('select', contact => {
 
                 Message.sendContact(
-                this._contactActive.chatId,
-                this._user.email,
-                contact
+                    this._contactActive.chatId,
+                    this._user.email,
+                    contact
                 )
 
             })
@@ -758,6 +786,18 @@ export class WhatsAppController {
         })
 
         this.el.btnFinishMicrophone.on('click', e => {
+
+            this._microphoneController.on('recorded', (file, metadata) => {
+
+                Message.sendAudio(
+                    this._contactActive.chatId,
+                    this._user.email,
+                    file,
+                    metadata,
+                    this._user.photo
+                )
+
+            })
 
             this._microphoneController.stopRecorder()
             this.closeRecordMicrophone()
